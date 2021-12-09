a6a07ffcf608c5ff3d2aafbc46265b22d81ca899
Carson Johnson <carson74johnson@gmail.com>
2021-12-09 12:07:19 -0500

    adding file structure and madlibs story

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_SharpMadLibs
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
