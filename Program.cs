b6f2036b11914e269e8f44a06c29e48a7611e756
Carson Johnson <carson74johnson@gmail.com>
2021-12-09 13:16:35 -0500

    * UserPrefs.xml: Added variables to the madliibs story and added   commenting for a skeleton
    * Program.cs: Adding variables to user story and added comments to
      build a little skeleton
    
    * C_Sharp-Mad-Libs-Debug|anycpu.json:

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace C_SharpMadLibs
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Init Variables
+
+
+            // Get Input from User
+            String madlibsStory =
+            $"{theMatrix} is a {system}, {neo}. That {system} is our {enemy}. " +
+            $"But when you're {inside}, you look around, what do you see? " +
+            $"{profession[0]}, {profession[1]}, {profession[2]}, {profession[3]}. " +
+            $"The very minds of the people we are trying to {save}. But until we do, " +
+            $"these people are still a part of that {system} and that makes " +
+            $"them our {enemy}. You have to understand, most of these people are " +
+            $"not ready to be {unplugged}. And many of them are so {adj[0]}, so " +
+            $"hopelessly {adj[0]} on the {system}, that they will {fight} to protect it.";
+
+            // Init Story
+
+            // Print Story
         }
     }
 }
