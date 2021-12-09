e2b7629e401066052c9481741d2950d363aa8a28
Carson Johnson <cjohnson74.tech@gmail.com>
2021-12-09 04:31:25 -0500

    Add files via upload

---
+++ C_Sharp-Mad-Libs/C_Sharp-Mad-Lib/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace C_SharpMadLibs
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Init Variables
+            string theMatrix, system, neo, enemy, inside, save, unplugged, fight;
+            string[] profession = new string[4], adj = new string[2];
+            
+            // Get Input From User
+            Console.WriteLine("Welcom user!");
+            Console.WriteLine("Let's Play a game of madlibs!");
+            Console.WriteLine("Please share with me your name?");
+            neo = Console.ReadLine();
+
+            // Init Story
+            // string madlibsStory = 
+            // $"{theMatrix} is a {system}, {neo}. That {system} is our {enemy}." +
+            // "But when you're {inside}, you look around, what do you see?" +
+            // "{profession[0]}, {profession[1]}, {profession[2]}, {profession[3]}. The very minds" +
+            // "of the people we are trying to {save}. But until we do," +
+            // "these people are still a part of that {system} and that makes" +
+            // "them our {enemy}. You have to understand, most of these people" +
+            // "are not ready to be {unplugged}. And many of them are so {adj[0]}," +
+            // "so hopelessly {adj[1]} on the {system}, that they will {fight} to protect it.";
+
+            // Print Story
+            Console.WriteLine(madlibsStory);
+        }
+    }
+}
